2d825d676c883fa8c158071c90f70be6501665ea
FrekeNobles <noblendifreke@gmail.com>
2026-04-28 18:25:37 +0100

    update: update next.config.ts to include support for pwa

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,27 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // Disable x-powered-by header
+  poweredByHeader: false,
+  
+  // Allow service worker
+  async headers() {
+    return [
+      {
+        source: '/sw.js',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=0, must-revalidate',
+          },
+          {
+            key: 'Service-Worker-Allowed',
+            value: '/',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
